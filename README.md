fc7e2135fc98d759fc671b084a193812caefed1b
Daniel Russom <daniel.russom00@gmail.com>
2017-12-22 20:10:35 +0000

    Init
    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Hard 4 Number Sequence Relations
+Find how a series of numbers add, subtract, multiply, and divide in ways that relate to each other.
+Prompt -
+https://www.reddit.com/r/dailyprogrammer/comments/pm7g7/2122012_challange_4_difficult/
---
+++ src/numbersequencerelations/Controller.java
@@ -0,0 +1,13 @@
+package numbersequencerelations;
+
+public class Controller {
+
+	public static void main(String[] args) {
+		// Initial problem sets
+		int[] problemSet1 = {4,2,8};
+		int[] problemSet2 = {6,2,12};
+		int[] problemSet3 = {6,2,3};
+		int[] problemSet4 = {9,12,108};
+		int[] problemSet5 = {4,16,64};
+	}
+}
